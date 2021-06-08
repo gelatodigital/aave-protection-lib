3a1459f170d25ff8c2a25184bfc7d19e3bc2e572
Twin Fish <matthieumariejoseph@gmail.com>
2021-06-08 16:55:16 +0200

    Feat/alpha version (#3)
    * alpha version of aave-protection-lib
    
    * change name of project
    
    * Chore refacto
    
    * add contract and addresses files

---
+++ src/types/contracts.ts
@@ -0,0 +1,27 @@
+import { ethers } from "ethers";
+import { AaveServices__factory } from "../contracts/types/factories/AaveServices__factory";
+import { AaveServices } from "../contracts/types/AaveServices";
+import { addresses } from "./addresses";
+import { Web3Provider } from "@ethersproject/providers";
+import { LendingPool } from "../contracts/types/LendingPool";
+import { LendingPool__factory } from "../contracts/types/factories/LendingPool__factory";
+import { ERC20 } from "../contracts/types/ERC20";
+import { ERC20__factory } from "../contracts/types/factories/ERC20__factory";
+
+const provider: ethers.providers.StaticJsonRpcProvider =
+  new ethers.providers.StaticJsonRpcProvider(process.env.REACT_APP_URL);
+
+export const getAaveServices = (provider: Web3Provider): AaveServices => {
+  return AaveServices__factory.connect(
+    addresses.AaveServices,
+    provider
+  ).connect(provider.getSigner());
+};
+
+export const getLendingPool = (provider: Web3Provider): LendingPool => {
+  return LendingPool__factory.connect(addresses.LendingPool, provider);
+};
+
+export const getERC20 = (provider: Web3Provider, tokenAddr: string): ERC20 => {
+  return ERC20__factory.connect(tokenAddr, provider);
+};
