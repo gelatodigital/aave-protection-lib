525d5ac7c07a22f330838f1287839ce2b300fc78
Shivva <matthieumariejoseph@gmail.com>
2021-06-28 16:36:56 +0200

    Change name and addr

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,8 +2,8 @@ import { Addresses } from "../types";
 
 const maticAddresses: Addresses = {
   Gelato: "0x7598e84B2E114AB62CAB288CE5f7d5f6bad35BbA",
-  AaveServices: "0x428Bf6548e8075e544624C05160B08811e76273f",
-  RefinanceAction: "0x885ea9fa41CfEBF3dEfeF0258e6804bbC76CE2ED",
+  AaveServices: "0xc9F785611986916f47f3A02590B17C3237A75b87",
+  ProtectionAction: "0xa4f5a143eda51075b33b7fc58d4be5c826605134",
   LendingPool: "0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf",
 };
 
