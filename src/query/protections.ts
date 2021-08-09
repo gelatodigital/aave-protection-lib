7afa258463caa959dd3ba9ffcd215c0cb1a850ed
Twin Fish <matthieumariejoseph@gmail.com>
2021-08-09 21:03:47 +0200

    Feat/is deprecated version (#18)

--- src/query/protections.ts
+++ src/query/protections.ts
@@ -1,25 +1,29 @@
 import {
   GET_ALL_CANCELLED_PROTECTION_BY_USER,
-  GET_ALL_SUBMITTED_PROTECTION_BY_USER,
-  GET_ALL_EXECUTED_PROTECTION_BY_USER,
+  GET_ALL_SUBMITTED_PROTECTION_BY_USER_AND_ACTION,
+  GET_ALL_EXECUTED_PROTECTION_BY_USER_AND_ACTION,
 } from "./graphql";
 import { request } from "graphql-request";
 import { subgraphUrl } from "../constants";
 import { Protection } from "../types";
 
-export const getSubmittedProtectionByUser = async (
-  user: string
+export const getSubmittedProtectionByUserAndAction = async (
+  user: string,
+  action: string
 ): Promise<Protection[]> => {
   const data = await request(
     subgraphUrl,
-    GET_ALL_SUBMITTED_PROTECTION_BY_USER,
+    GET_ALL_SUBMITTED_PROTECTION_BY_USER_AND_ACTION,
     {
       user: user.toLowerCase(),
+      action: action.toLowerCase(),
     }
   );
-  if (!data) throw new Error("getSubmittedProtectionByUser: NO DATA");
+  if (!data) throw new Error("getSubmittedProtectionByUserAndAction: NO DATA");
   if (!data.protections)
-    throw new Error("getSubmittedProtectionByUser: NO PROTECTIONS FIELD");
+    throw new Error(
+      "getSubmittedProtectionByUserAndAction: NO PROTECTIONS FIELD"
+    );
   return data.protections;
 };
 
@@ -39,14 +43,22 @@ export const getCancelledProtectionByUser = async (
   return data.protections;
 };
 
-export const getExecutedProtectionByUser = async (
-  user: string
+export const getExecutedProtectionByUserAndAction = async (
+  user: string,
+  action: string
 ): Promise<Protection[]> => {
-  const data = await request(subgraphUrl, GET_ALL_EXECUTED_PROTECTION_BY_USER, {
-    user: user.toLowerCase(),
-  });
-  if (!data) throw new Error("getExecutedProtectionByUser: NO DATA");
+  const data = await request(
+    subgraphUrl,
+    GET_ALL_EXECUTED_PROTECTION_BY_USER_AND_ACTION,
+    {
+      user: user.toLowerCase(),
+      action: action.toLowerCase(),
+    }
+  );
+  if (!data) throw new Error("getExecutedProtectionByUserAndAction: NO DATA");
   if (!data.protections)
-    throw new Error("getExecutedProtectionByUser: NO PROTECTIONS FIELD");
+    throw new Error(
+      "getExecutedProtectionByUserAndAction: NO PROTECTIONS FIELD"
+    );
   return data.protections;
 };
